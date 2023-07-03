f057258a19d32261cc691f3debded4379f21d95d
Tarun Mahajan <2922986+tango4567@users.noreply.github.com>
2023-07-04 00:10:13 +0530

    updated test and changelogs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.0.2
+
+* TODO: In this version we added image but not optimized. We are working on it and resolving asap.
+* TODO: In this version we added image but not optimized. We are working on it and resolving asap.
+
 ## 0.0.1
 
-* TODO: Describe initial release.
+* TODO: this is template version with dummy data.
--- test/pexels_photos_videos_test.dart
+++ test/pexels_photos_videos_test.dart
@@ -3,10 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pexels_photos_videos/pexels_photos_videos.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('pass your own authorization key and image id to run this test', () {
+    final pexelsMedia = PexelsMedia();
+    expect(pexelsMedia.getPexelsPhotos('authorizationKey', 'imageId'),
+        'Invalid Authentication');
   });
 }
