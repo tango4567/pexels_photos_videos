8225e32dcc718b932184fdfb30aad587f6c9f73e
Tarun Mahajan <2922986+tango4567@users.noreply.github.com>
2023-07-04 05:43:56 +0530

    Documantation updated

--- lib/pexels_media.dart
+++ lib/pexels_media.dart
@@ -5,32 +5,38 @@ import 'package:http/http.dart' as http;
 
 import 'pexels_result.dart';
 
+///[PexelsMedia] is having multiple methods
 class PexelsMedia {
+  ///Static variable are protected variable
+  static const String _authorization = 'Authorization';
+  static const String _contentType = 'Content-Type';
+  static const String _contentTypeDefinition =
+      'application/json; charset=UTF-8';
 
-  static const String authorization = 'Authorization';
-  static const String contentType = 'Content-Type';
-  static const String contentTypeDefinition = 'application/json; charset=UTF-8';
+  static const String _pexelsApiBaseUrl = 'api.pexels.com';
+  static const String _pexelsPhoto = 'v1/photos';
+  static const String _pexelsVideos = 'v1/videos';
 
-  static const String pexelsApiBaseUrl = 'api.pexels.com';
-  static const String pexelsPhoto = 'v1/photos';
-  static const String pexelsVideos = 'v1/videos';
-
-
+  ///[getPexelsPhotos] required to parameters
+  ///1. Pexels => [authorizationKey]
+  ///2. Image ID => [imageId]
+  ///
   Future<PexelsResult> getPexelsPhotos(
       String authorizationKey, String imageId) async {
-    final url = Uri.https(
-        pexelsApiBaseUrl, '$pexelsPhoto/$imageId');
+    final url = Uri.https(_pexelsApiBaseUrl, '$_pexelsPhoto/$imageId');
 
-    final response = await http.get(url,
-        headers: {
-          contentType: contentTypeDefinition,
-          authorization: authorizationKey
-        });
+    /// Get response from Pexels.com
+    final response = await http.get(url, headers: {
+      _contentType: _contentTypeDefinition,
+      _authorization: authorizationKey
+    });
 
+    /// If [response.statusCode != 200] then it will throw exception
     if (response.statusCode != 200) {
       throw Exception('Exception ${response.statusCode}');
     }
 
+    /// Here we are converting [response] data to [PexelsResult]
     final packageJson = json.decode(response.body) as Map<String, dynamic>;
     return PexelsResult.fromJson(packageJson);
   }
